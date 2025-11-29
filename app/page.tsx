547f27409ea7f5c2bca7ce67f6672a775ce73c48
Betty Godier <engineer@betty.dev>
2025-11-29 19:41:58 +0100

    Call internal API from homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,23 @@
 import Image from "next/image";
 
-export default function Home() {
+async function getHello() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/hello`);
+  if (!res.ok) {
+    return { message: "Erreur API", timestamp: "" };
+  }
+  return res.json();
+}
+
+export default async function Home() {
+  const data = await getHello();
+
   return (
     <main style={{ padding: 40, fontFamily: "system-ui" }}>
       <h1>Flex Gateway – MVP</h1>
-      <p>
-        Ceci est la première version de ma plateforme de flexibilité pour la
-        recharge de poids lourds.
-      </p>
+      <p>Ceci est la première version de ma plateforme de flex.</p>
+
+      <h2>Test API interne</h2>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </main>
   );
 }
